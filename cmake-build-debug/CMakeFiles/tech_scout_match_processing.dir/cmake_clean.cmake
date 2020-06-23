1e1a3d0590ef501cc33bbcc7ab534f7f04296a22
Anas Merbouh <anas.merbouh@outlook.com>
2020-06-23 17:26:58 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/tech_scout_match_processing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tech_scout_match_processing.dir/main.cpp.o"
+  "tech_scout_match_processing"
+  "tech_scout_match_processing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tech_scout_match_processing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
